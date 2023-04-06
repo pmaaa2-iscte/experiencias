f15877f77f6bcb64df8e5e4f97bcb0004d6c2a5a
pmaaa2-iscte <pmaaa2@iscte-iul.pt>
2023-04-06 12:56:50 +0100

    ISTO E O TITULO
    ajdlsjdfos

---
+++ experienciasvideo/src/main/java/pmaaa2_iscte/experienciasvideo/App.java
@@ -0,0 +1,13 @@
+package pmaaa2_iscte.experienciasvideo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
